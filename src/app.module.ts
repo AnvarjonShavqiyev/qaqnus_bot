9f30d2b9c4d089cb9e0e293e6ef5ab50bd385a1c
Anvarjon Shavqiyev <a.shavqiyev@itransition.com>
2025-11-14 14:05:02 +0500

    healthy route added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
 import { BotModule } from './bot/bot.module';
 import { NotificationModule } from './notifications/notification.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [BotModule, NotificationModule],
+  controllers: [AppController],
   providers: [PrismaService],
 })
 export class AppModule {}
